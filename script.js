3e353bc7f38a76467677253f1b92223282dbf301
shamineeanne23-web <shamineeanne23@gmail.com>
2026-08-05 11:14:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function findSeat() {
+    const name = document.getElementById("searchInput").value.trim();
 
+    if (name === "") {
+        alert("Please enter your child's full name.");
+        return;
+    }
+
+    const student = students.find(s =>
+        s.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (student) {
+        localStorage.setItem("student", JSON.stringify(student));
+        window.location.href = "result.html";
+    } else {
+        alert("Sorry, we couldn't find that name.");
+    }
+}
